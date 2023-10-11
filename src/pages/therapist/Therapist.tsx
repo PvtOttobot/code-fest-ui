60112613cd7fe49dff4d64fc651a6ffc85d2d349
PaulMcFet <paulmcfetridge1993@gmail.com>
2023-10-11 22:40:52 +0100

    PMadded way to arrange visits by time

--- src/pages/therapist/Therapist.tsx
+++ src/pages/therapist/Therapist.tsx
@@ -2,14 +2,21 @@ import {Appointment} from "./appointment/Appointment.tsx";
 
 const Therapist = () => {
     const appts = [
-        {name:'Safia Young',date:'11/11/11', time:'13:00'},
-        {name:'Natalia Combs',date:'11/11/11',time:'14:00'},
-        {name:'Johnathan Prince',date:'11/11/11',time:'14:30'},
+        {name:'Safia Young',date:'11/11/11', time:'17:00'},
+        {name:'Natalia Combs',date:'11/11/11',time:'10:00'},
+        {name:'Johnathan Prince',date:'11/11/11',time:'09:30'},
     ]
+        appts.sort(function (a, b) {
+        return a.time.localeCompare(b.time);
+    });
+    
+    console.log(appts);
+    
     return(<div className="p-4 bg-white h-screen space-y-6">
         {
             appts.map((appt) => (
                 <>
+                
                     <Appointment date={appt.date} name={appt.name} time={appt.time}></Appointment>
                 </>
 
