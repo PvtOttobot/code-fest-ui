1b6f24691fcc092af62e49007528590c267250c5
Daniel Otto Petersen <danielottojackpetersen2@gmail.com>
2023-10-12 11:31:20 +0100

    feat(therapist): more features

--- src/pages/therapist/Therapist.tsx
+++ src/pages/therapist/Therapist.tsx
@@ -1,20 +1,41 @@
 import {Appointment} from "./appointment/Appointment.tsx";
+import {useState} from "react";
 
 const Therapist = () => {
-    const appts = [
-        {name:'Safia Young',date:'11/11/11', time:'13:00', postcode:'A12 3BC'},
-        {name:'Natalia Combs',date:'11/11/11',time:'14:00', postcode:'A12 3BC'},
-        {name:'Johnathan Prince',date:'11/11/11',time:'14:30', postcode:'A12 3BC'},
-        {name:'Safia Young',date:'11/11/11', time:'13:00', postcode:'A12 3BC'},
-        {name:'Natalia Combs',date:'11/11/11',time:'14:00', postcode:'A12 3BC'},
-        {name:'Johnathan Prince',date:'11/11/11',time:'14:30', postcode:'A12 3BC'},
-        {name:'Safia Young',date:'11/11/11', time:'13:00', postcode:'A12 3BC'},
-        {name:'Natalia Combs',date:'11/11/11',time:'14:00', postcode:'A12 3BC'},
-        {name:'Johnathan Prince',date:'11/11/11',time:'14:30', postcode:'A12 3BC'},
+    const apptsFake = [
+        {name:'Safia Young',date:'11/11/11', time:'13:00', postcode:'A12 3BC', large: false},
+        {name:'Natalia Combs',date:'11/11/11',time:'14:00', postcode:'A12 3BC', large: false},
+        {name:'Johnathan Prince',date:'11/11/11',time:'14:30', postcode:'A12 3BC', large: false},
+        {name:'Safia Young',date:'11/11/11', time:'13:00', postcode:'A12 3BC', large: false},
+        {name:'Natalia Combs',date:'11/11/11',time:'14:00', postcode:'A12 3BC', large: false},
+        {name:'Johnathan Prince',date:'11/11/11',time:'14:30', postcode:'A12 3BC', large: false},
+        {name:'Safia Young',date:'11/11/11', time:'13:00', postcode:'A12 3BC', large: false},
+        {name:'Natalia Combs',date:'11/11/11',time:'14:00', postcode:'A12 3BC', large: false},
+        {name:'Johnathan Prince',date:'11/11/11',time:'14:30', postcode:'A12 3BC', large: false},
     ]
+    const [appts, setAppts] = useState(apptsFake);
+
+    const expand = (id: string) => {
+        console.log("TODO expanded this id "+id)
+        setAppts([
+            {name:'Safia Young',date:'11/11/11', time:'13:00', postcode:'A12 3BC', large: true},
+        ])
+    }
+    const collapse = () => {
+        console.log("TODO collapsed this")
+        setAppts(apptsFake)
+    }
     return(<div className="p-4 bg-amber-white h-screen space-y-6 overflow-auto">
         {appts.map((appt) => (
-            <Appointment date={appt.date} name={appt.name} time={appt.time} postcode={appt.postcode}></Appointment>
+            <Appointment
+                date={appt.date}
+                name={appt.name}
+                time={appt.time}
+                postcode={appt.postcode}
+                expand={expand}
+                collapse={collapse}
+                large={appt.large}
+            ></Appointment>
         ))}
     </div>);
 }
--- src/pages/therapist/appointment/Appointment.tsx
+++ src/pages/therapist/appointment/Appointment.tsx
@@ -1,37 +1,95 @@
 import {ArrowsPointingOutIcon} from "@heroicons/react/24/outline";
+import {ArrowsPointingInIcon} from "@heroicons/react/20/solid";
+import {useEffect, useState} from "react";
 
 interface AppointmentProps {
     name: string;
     date: string;
     time: string;
     postcode: string;
+    large?: boolean;
+    expand: (id: string) => {};
+    collapse: () => {};
 }
 
 const Appointment = (props: AppointmentProps) => {
-    return(
-        <button className="p-4 bg-blue-300 w-full flex flex-row gap-4">
-            <div className="grow text-left">
-                <p className="text-blue-950 font-bold">
-                    {props.name}
-                </p>
-                <p className="text-blue-950 font-bold">
-                    {props.postcode}
-                </p>
-                <p className="text-blue-950 font-bold">
-                    {props.date}
-                </p>
-                <p className="text-blue-950 font-bold">
-                    {props.time}
-                </p>
+    const [counter, setCounter] = useState(3000);
+    const [checkedIn, setCheckedIn] = useState(false)
+
+    useEffect(() => {
+        checkedIn && counter > 0 && setTimeout(() => setCounter(counter - 1), 1000);
+    }, [counter, checkedIn]);
+    const expand = () => {
+        props.expand(props.name)
+    }
+    const emergencyAlert = () => {
+        confirm("WARNING: Send an Emergency Alert now?");
+    }
+
+    if (!props.large) {
+        return(
+            <button onClick={expand} className="p-4 bg-blue-300 w-full flex flex-row gap-4">
+        <div className="grow text-left">
+            <p className="text-blue-950 font-bold">
+                {props.name}
+            </p>
+            <p className="text-blue-950 font-bold">
+                {props.postcode}
+            </p>
+            <p className="text-blue-950 font-bold">
+                {props.date}
+            </p>
+            <p className="text-blue-950 font-bold">
+                {props.time}
+            </p>
+        </div>
+        <div>
+            <p>
+                <span className="hidden sm:inline">Expand </span>
+                <ArrowsPointingOutIcon className="h-6 inline" ></ArrowsPointingOutIcon>
+            </p>
+        </div>
+    </button>
+        )
+    } else {
+        return(
+            <div className=" h-full p-4 bg-blue-300 flex flex-col gap-4">
+                <div className="w-full flex flex-row gap-4 grow">
+                    <div className="grow text-left flex flex-col gap-4">
+                        <h1 className="text-blue-950 text-2xl font-bold">
+                            {props.name}
+                        </h1>
+                        <p className="text-blue-950 font-bold">
+                            12 Fake Street<br/>
+                            Glasgow<br/>
+                            {props.postcode}
+                        </p>
+                        <p className="text-blue-950 font-bold">
+                            {props.date}<br/>
+                            {props.time}<br/>
+                        </p>
+
+                    </div>
+                    <div className="">
+                        <button onClick={props.collapse}>
+                            <span className="hidden sm:inline">Minimize </span>
+                            <ArrowsPointingInIcon className="h-6 inline" ></ArrowsPointingInIcon>
+                        </button>
+                    </div>
+                </div>
+                {checkedIn && <div className="text-center text-4xl">
+                    <p>{Math.floor(counter / 60)}:{Math.floor(counter % 60)}</p>
+                </div>}
+
+                {!checkedIn && <button className="bg-blue-600 text-white p-2 rounded-md" onClick={() => setCheckedIn(true)}>Check In</button>}
+                {checkedIn && <button className="bg-white text-black p-2 rounded-md" onClick={() => setCheckedIn(false)}>Check Out</button>}
+                <div className="flex flex-row gap-4">
+                    <button className="bg-red-700 text-white p-2 w-full rounded-md" onClick={emergencyAlert}>Emergency</button>
+                    <button className="bg-blue-600 text-white p-2 w-full rounded-md">More</button>
+                </div>
             </div>
-            <div className="">
-                <p className="hi">
-                    <span className="hidden sm:inline">Expand </span>
-                    <ArrowsPointingOutIcon className="h-6 inline" ></ArrowsPointingOutIcon>
-                </p>
-            </div>
-        </button>
-    );
+        )
+    }
 }
 
 export { Appointment };
