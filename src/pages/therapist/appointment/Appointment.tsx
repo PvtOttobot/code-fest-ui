404cc1d05b81cfaf139fba0d30eaa89640696965
Daniel Otto Petersen <danielottojackpetersen2@gmail.com>
2023-10-12 07:38:55 +0100

    feat(therapist): add icons and refine therapist styling

--- src/pages/therapist/appointment/Appointment.tsx
+++ src/pages/therapist/appointment/Appointment.tsx
@@ -1,22 +1,36 @@
+import {ArrowsPointingOutIcon} from "@heroicons/react/24/outline";
+
 interface AppointmentProps {
     name: string;
     date: string;
     time: string;
+    postcode: string;
 }
 
 const Appointment = (props: AppointmentProps) => {
     return(
-        <div className="p-4 bg-blue-300">
-            <p className="text-blue-950 font-bold">
-                Name: {props.name}
-            </p>
-            <p className="text-blue-950 font-bold">
-                Date: {props.date}
-            </p>
-            <p className="text-blue-950 font-bold">
-                Time: {props.time}
-            </p>
-        </div>
+        <button className="p-4 bg-blue-300 w-full flex flex-row gap-4">
+            <div className="grow text-left">
+                <p className="text-blue-950 font-bold">
+                    {props.name}
+                </p>
+                <p className="text-blue-950 font-bold">
+                    {props.postcode}
+                </p>
+                <p className="text-blue-950 font-bold">
+                    {props.date}
+                </p>
+                <p className="text-blue-950 font-bold">
+                    {props.time}
+                </p>
+            </div>
+            <div className="">
+                <p className="hi">
+                    <span className="hidden sm:inline">Expand </span>
+                    <ArrowsPointingOutIcon className="h-6 inline" ></ArrowsPointingOutIcon>
+                </p>
+            </div>
+        </button>
     );
 }
 
